6f1c0897dd09485786677993a0428da8779ea567
jchamber <jchamber@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2006-11-27 11:35:20 +0000

    final changes for refactoring of activity dialog box

--- src/opendap/clients/odc/Activity.java
+++ src/opendap/clients/odc/Activity.java
@@ -20,7 +20,8 @@ public class Activity extends Thread {
 	final static JPanel jpanelGlassPane = (JPanel)ApplicationController.getInstance().getAppFrame().getGlassPane();
 	final static JButton jbuttonCancelActivity = new JButton("Cancel");
 	static MouseAdapter mmouseadapterPopupCancel;
-	static JPanel jpanelActivityDialog = new JPanel();
+//	static JPanel jpanelActivityDialog = new JPanel();
+	static JDialog dialogActivity;
 
 	static {
 		Dimension dimScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -42,23 +43,46 @@ public class Activity extends Thread {
 
 		jpanelGlassPane.setLayout( null );
 
-		jpanelActivityDialog.setBorder( BorderFactory.createLineBorder(Color.BLUE) );
+		// the button container is needed because the there is a bug in Swing that causess the
+		// dialog sizing to not take into account the title height so the button gets cutoff
+		final JPanel panelButtonContainer = new JPanel();
+		panelButtonContainer.setLayout( new BoxLayout(panelButtonContainer, BoxLayout.Y_AXIS) );
+		panelButtonContainer.add( jbuttonCancelActivity );
+		panelButtonContainer.add( Box.createVerticalStrut(15) );
+		final Object[] aOptionPaneButtons = new Object[1];
+		aOptionPaneButtons[0] = panelButtonContainer;
 
-		jpanelActivityDialog.setLayout( new BoxLayout(jpanelActivityDialog, BoxLayout.Y_AXIS) );
-		jpanelActivityDialog.setBorder( new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, Color.lightGray, Color.darkGray ));
+		final JOptionPane optionPane = new JOptionPane(
+						jtaMessage,
+						JOptionPane.PLAIN_MESSAGE,
+						JOptionPane.NO_OPTION,
+						null,
+						aOptionPaneButtons
+		);
 
-		jpanelActivityDialog.setSize( new Dimension(400, 200) );
+		dialogActivity = new JDialog(
+				ApplicationController.getInstance().getAppFrame(),
+				sTitle,
+				false);
+		dialogActivity.setContentPane(optionPane);
 
-		jpanelActivityDialog.add( jtaMessage );
-		jpanelActivityDialog.add( Box.createVerticalStrut(15) );
-		jpanelActivityDialog.add( jbuttonCancelActivity );
-
-		jpanelGlassPane.add(jpanelActivityDialog, BorderLayout.CENTER);
-		jpanelGlassPane.add(Box.createVerticalStrut(200), BorderLayout.NORTH);
-		jpanelGlassPane.add(Box.createGlue(), BorderLayout.WEST);
-		jpanelGlassPane.add(Box.createGlue(), BorderLayout.EAST);
-		jpanelGlassPane.add(Box.createGlue(), BorderLayout.SOUTH);
-		jtaMessage.addMouseListener(mmouseadapterPopupCancel);
+//		jpanelActivityDialog.setBorder( BorderFactory.createLineBorder(Color.BLUE) );
+//
+//		jpanelActivityDialog.setLayout( new BoxLayout(jpanelActivityDialog, BoxLayout.Y_AXIS) );
+//		jpanelActivityDialog.setBorder( new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, Color.lightGray, Color.darkGray ));
+//
+//		jpanelActivityDialog.setSize( new Dimension(400, 200) );
+//
+//		jpanelActivityDialog.add( jtaMessage );
+//		jpanelActivityDialog.add( Box.createVerticalStrut(15) );
+//		jpanelActivityDialog.add( jbuttonCancelActivity );
+//
+//		jpanelGlassPane.add(jpanelActivityDialog, BorderLayout.CENTER);
+//		jpanelGlassPane.add(Box.createVerticalStrut(200), BorderLayout.NORTH);
+//		jpanelGlassPane.add(Box.createGlue(), BorderLayout.WEST);
+//		jpanelGlassPane.add(Box.createGlue(), BorderLayout.EAST);
+//		jpanelGlassPane.add(Box.createGlue(), BorderLayout.SOUTH);
+//		jtaMessage.addMouseListener(mmouseadapterPopupCancel);
 
 	}
 	static int mctCurrentActivities = 0;
@@ -67,16 +91,15 @@ public class Activity extends Thread {
 	private boolean mzCancelled = false;
 
 	public void setMessageText( String sText ){
+		/*
 		Dimension dimScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		double dScreenHeight = dimScreenSize.getHeight();
 		double dScreenWidth  = dimScreenSize.getWidth();
-		FontMetrics fm = jpanelActivityDialog.getGraphics().getFontMetrics( fontMessage );
+		FontMetrics fm = dialogActivity.getGraphics().getFontMetrics( fontMessage );
 		int len = fm.stringWidth( sText );
 		int iDialogWidth;
 		int iDialogHeight;
 		iDialogWidth = (int) (dScreenWidth * 0.80 + 50d);
-		System.out.println("screen width: " + dScreenWidth);
-		System.out.println("len: " + len);
 		if( len < dScreenWidth * 0.80 ){
 			iDialogHeight = 80;
 		} else if( len < dScreenWidth * 4 ) {
@@ -86,9 +109,9 @@ public class Activity extends Thread {
 		} else {
 			iDialogHeight = 400;
 		}
-		System.out.println("height: " + iDialogHeight);
-		jpanelActivityDialog.setMaximumSize( new Dimension( iDialogWidth, iDialogHeight) );
-		jpanelActivityDialog.setLocation( 200, 200 );
+		*/
+//		dialogActivity.setMaximumSize( new Dimension( iDialogWidth, iDialogHeight) );
+//		dialogActivity.setLocation( 200, 200 );
 		jtaMessage.setText( sText );
 	}
 
@@ -136,6 +159,7 @@ public class Activity extends Thread {
 			new Runnable(){
 				public void run(){
 					jpanelGlassPane.setVisible(false);
+					dialogActivity.setVisible(false);
 				}
 			}
 		);
@@ -171,7 +195,10 @@ public class Activity extends Thread {
 										vCancelActivity();
 									}
 								 };
-							jpanelGlassPane.setVisible(true);
+							dialogActivity.setLocationRelativeTo(ApplicationController.getInstance().getAppFrame());
+							dialogActivity.pack();
+							dialogActivity.setVisible(true);
+							// jpanelGlassPane.setVisible(true);
 						}
 					}
 				);
@@ -232,8 +259,9 @@ public class Activity extends Thread {
 					SwingUtilities.invokeLater(
 						new Runnable(){
 							public void run(){
-								jpanelGlassPane.remove(jtaMessage);
-								jpanelGlassPane.setVisible(false);
+//								jpanelGlassPane.remove(jtaMessage);
+//								jpanelGlassPane.setVisible(false);
+								dialogActivity.setVisible(false);
 							}
 						}
 					);
