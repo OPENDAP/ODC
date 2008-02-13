d741eac78d7f7dbb7a7dbc9a2c8c62d5ffa355cc
ndp <ndp@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2008-02-13 17:37:23 +0000

    ODC: Updated Java DAP library to alpha version 0.0.8 (openda-0.0.8.jar) Added Apache Commons HttpClient library (commons-httpclient-3.0.1.jar) Changed OpendapConnection to use new ServerVersion constructor. Changed Catalog to use DConnect2

--- src/opendap/clients/odc/OpendapConnection.java
+++ src/opendap/clients/odc/OpendapConnection.java
@@ -215,7 +215,14 @@ public class OpendapConnection {
 				try { socket_channel.close(); } catch( Throwable t_is ){}
 				return null;
 			}
-			ver = new ServerVersion(sServerVersion);
+            try{
+                ver = new ServerVersion(sServerVersion,ServerVersion.XDODS_SERVER);
+            }
+            catch(opendap.dap.DAP2Exception e){
+                sbError.append("not a valid OPeNDAP server, cannot parse field xdods-server header: "+sServerVersion);
+                try { socket_channel.close(); } catch( Throwable t_is ){}
+                return null;
+            }
 			String sTransferEncoding = getHeaderField(sHeader, "transfer-encoding");
 			if( sTransferEncoding != null ){
 				if( sProtocol.equals("HTTP/1.0") ){
