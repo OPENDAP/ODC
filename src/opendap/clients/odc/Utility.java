242fe45a0e9425b5b38c9fc3b99064fbdcf09190
jchamber <jchamber@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2009-05-15 21:17:33 +0000

    segregated array utility functions

--- src/opendap/clients/odc/Utility.java
+++ src/opendap/clients/odc/Utility.java
@@ -1576,87 +1576,10 @@ ScanForStartOfMatch:
 		return adTransformedValues0;
 	}
 
-	public static String dumpArray( int[] ai, int from, int to ){
-		if( ai == null ) return "[null]";
-		if( to == 0 ) to = ai.length - 1;
-		StringBuffer sb = new StringBuffer(80);
-		for( int x = from; x <= to; x++ ){
-			sb.append("[" + x + "] = " + ai[x] + "\n");
-		}
-		return sb.toString();
-	}
-	public static String dumpArray( String[] as, int from, int to ){
-		if( as == null ) return "[null]";
-		if( from < 0 ) from = 0;
-		if( to == 0 || to > as.length - 1 ) to = as.length - 1;
-		StringBuffer sb = new StringBuffer(80);
-		for( int x = from; x <= to; x++ ){
-			sb.append("[" + x + "] = " + as[x] + "\n");
-		}
-		return sb.toString();
-	}
 	public static boolean equalsUPPER( String s1, String s2 ){
 		if( s1 == null || s2 == null ) return false;
 		return s1.equalsIgnoreCase(s2);
 	}
-	public static boolean arrayHasMember( int[] ai, int value ){
-		for( int x = 0; x < ai.length; x++ ) if( ai[x] == value ) return true;
-		return false;
-	}
-	public static boolean arrayHasMember( String[] as, String s ){
-		for( int x = 0; x < as.length; x++ ) if( as[x] == null ? s == null : as[x].equalsIgnoreCase( s ) ) return true;
-		return false;
-	}
-	public static boolean arrayStartsWithMember( String[] as, String s ){
-		for( int x = 0; x < as.length; x++ ) if( as[x] == null ? s == null : as[x].startsWith( s ) ) return true;
-		return false;
-	}
-	public static String dumpArray( double[] ad, int from, int to ){
-		if( ad == null ) return "null";
-		if( to == 0 ) to = ad.length - 1;
-		StringBuffer sb = new StringBuffer(80);
-		for( int x = from; x <= to; x++ ){
-			sb.append("[" + x + "] = " + ad[x] + "\n");
-		}
-		return sb.toString();
-	}
-	public static String dumpArray( float[] af, int from, int to ){
-		if( af == null ) return "null";
-		if( to == 0 ) to = af.length - 1;
-		StringBuffer sb = new StringBuffer(80);
-		for( int x = from; x <= to; x++ ){
-			sb.append("[" + x + "] = " + af[x] + "\n");
-		}
-		return sb.toString();
-	}
-	public static String sDumpMatrix( double[] ai, int x_len, int y_len, int x_to, int y_to ){
-		int xValue = -1;
-		StringBuffer sb = new StringBuffer(250);
-		for( int xY = 1; xY <= y_len; xY++ ){
-			if( xY > y_to ) break;
-			for( int xX = 1; xX <= x_len; xX++ ){
-				xValue++;
-				if( xX > x_to ) continue;
-				sb.append(' ');
-				if( ai[xValue] < 10 ) sb.append(' ');
-				sb.append(ai[xValue]);
-			}
-		    sb.append("\n");
-		}
-		return sb.toString();
-	}
-
-	static String sDumpMatrix( int[] ai ){
-		StringBuffer sb = new StringBuffer(250);
-		int lenArray = ai.length;
-		sb.append("array[" + lenArray + "]: ");
-		for( int x = 0; x < lenArray; x++ ){
-			sb.append(' ');
-			sb.append(ai[x]);
-		}
-		return sb.toString();
-	}
-
 	public final static boolean zNullOrBlank( String s ){
 		if( s == null ) return true;
 		if( s.trim().length() == 0 ) return true;
