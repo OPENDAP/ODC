9385def148beb24c84a4b2d18d2ea68b9a96f372
jchamber <jchamber@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2011-05-03 22:02:27 +0000

    array indexing work

--- src/opendap/clients/odc/data/Model_VariableView.java
+++ src/opendap/clients/odc/data/Model_VariableView.java
@@ -13,4 +13,11 @@ public class Model_VariableView {
 	public int dim_column = 2;
 	public int[] page = new int[10];
 	public String sLastSelectedValue = null; // value of the last cell where Ctrl+arrow key was pressed
+	
+	// these constants are used to index values in the primitive vector
+	// for example the array time[hours][minutes][seconds] has constants 60 and 60 and 1
+	// if hours are rows and minutes are columns
+	public int index_factor_row = 0;
+	public int index_factor_column = 0;
+	public int index_factor_remainder = 0; // this number is added at the end
 }
