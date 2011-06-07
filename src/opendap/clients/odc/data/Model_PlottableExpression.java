acaa2ac1cf1a210462c32337aa1d1822511739d8
jchamber <jchamber@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2011-06-07 17:51:52 +0000

    reorganization of script related code, script to its own file

--- src/opendap/clients/odc/data/Model_PlottableExpression.java
+++ src/opendap/clients/odc/data/Model_PlottableExpression.java
@@ -1,10 +1,29 @@
 package opendap.clients.odc.data;
 
-/** Models a plottable expression such as "y=x^"
+/** Models a plottable expression such as "y=x^2"
  *  Created from a plottable script. See "Plotting Expressions" in the help text.
  *  See opendap.clients.odc.Interpreter.generatePlottableExpression() for generation code.
  */
 
 public class Model_PlottableExpression {
+	private PlottableExpression_TYPE type;
+	private Model_PlottableExpression(){}
+	PlottableExpression_TYPE getType(){ return type; } 
+	final static Model_PlottableExpression create( Script script, StringBuffer sbError ){
+		Model_PlottableExpression model = new Model_PlottableExpression();
+		
+		String sScriptText = script.getText();
+		// gather symbols
+		// convert symbols
+		// determine type of expression
+		// make compiled objects
+		
+		return model;
+	}
+}
 
+enum PlottableExpression_TYPE {
+	Cartesian,
+	Polar,
+	Parametric
 }
